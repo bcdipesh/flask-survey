afa09ba3abd0bf5b0fd5479832ca8e6f564ead58
bc-dipesh <bcdipeshwork@gmail.com>
2023-04-03 09:02:48 -0400

    Implement flask session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,19 +7,29 @@ app.config["SECRET_KEY"] = "survey app"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-# responses of people answer to the survey questions
-responses = []
-
 
 @app.route('/')
 def home_page():
     """Handle how to display home page"""
+
     return render_template('index.html', satisfaction_survey=satisfaction_survey)
+
+
+@app.route('/start-survey', methods=['POST'])
+def start_survey():
+    """Sets a session and starts the survey"""
+
+    # responses of people answer to the survey questions
+    session['responses'] = []
+
+    return redirect('/questions/0')
 
 
 @app.route('/questions/<int:question_num>')
 def display_question(question_num):
     """Handle how to display question"""
+
+    responses = session['responses']
 
     # protect questions route
     if (question_num == len(responses)):
@@ -28,6 +38,7 @@ def display_question(question_num):
         flash("Trying to access invalid question", "Error")
         return redirect('/thank-you')
     else:
+        print("inside elif")
         flash("Trying to access invalid question", "Error")
         return redirect(f'/questions/{len(responses)}')
 
@@ -35,9 +46,11 @@ def display_question(question_num):
 @app.route('/answer', methods=['POST'])
 def save_user_answer():
     """Handle how user answer is saved"""
-    answer = request.form['answer']
 
+    answer = request.form['answer']
+    responses = session['responses']
     responses.append(answer)
+    session['responses'] = responses
 
     # if all questions are answered redirect to thank you page
     if (len(responses) == len(satisfaction_survey.questions)):
